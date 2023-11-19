9d8d0ffc930942652f09108f916a9e827e8dfc40
Abdul Rahman Hamidy <arahmancsd@gmail.com>
2023-11-19 18:50:41 +0000

    Initial commit

---
+++ MauiShellContentName/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiShellContentName;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+
+	// public ShellContent MainPage => DeviceInfo.Platform == DevicePlatform.Android ? pageAndroid : pageiOS;
+	// public ShellContent PageOne => DeviceInfo.Platform == DevicePlatform.Android ? page1Android : page1iOS;
+}
---
+++ MauiShellContentName/Page1.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiShellContentName;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
+
